8e6c05825090711bc9c8e29c71a2899900c7a263
Igor Olenitsj <igor.olenitsj@traxion.com>
2020-12-04 19:31:14 +0100

    feat: day 3 part 1

---
+++ com.olenitsj.adventofcode/Day3.fs
@@ -0,0 +1,13 @@
+﻿module Day3
+
+open System.IO
+let config  = [(1,1);(1,3);(1,5);(1,7);(2,1)]
+let finalIndex steps listLength = steps * 3 % listLength
+
+
+let day3 = 
+    seq { yield! File.ReadLines @"DayThreeInput.txt" } 
+    |> Seq.indexed 
+    |> Seq.map (fun (i,e) -> Seq.skip (finalIndex i e.Length) e |> Seq.head ) 
+    |> Seq.filter (fun a -> a = '#') 
+    |> Seq.length
--- com.olenitsj.adventofcode/Program.fs
+++ com.olenitsj.adventofcode/Program.fs
@@ -2,10 +2,12 @@
 open Day1
 open Day2_1
 open Day2_2
+open Day3
 
 [<EntryPoint>]
 let main argv =
    printfn "Day 1: %i" <| day1 2020 3
    printfn "Day 2_1: %A" <| day2_1
    printfn "Day 2_2: %A" <| day2_2
+   printfn "Day 3_1: %A" <| day3
    0 // return an integer exit code
