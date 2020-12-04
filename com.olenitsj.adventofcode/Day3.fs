abd70081990becf859fbbdf3e8de4f58d9b35409
Igor Olenitsj <igor.olenitsj@traxion.com>
2020-12-04 23:39:29 +0100

    refactor: extract input

--- com.olenitsj.adventofcode/Day3.fs
+++ com.olenitsj.adventofcode/Day3.fs
@@ -3,16 +3,17 @@
 open System.IO
 let config  = [(1,1);(1,3);(1,5);(1,7);(2,1)]
 let finalIndex steps stepsize listLength = steps * stepsize % listLength
+let input = seq { yield! File.ReadLines @"DayThreeInput.txt" }
 
 let day3 = 
     config 
-    |> Seq.map (fun (down, right) ->  
-        seq { yield! File.ReadLines @"DayThreeInput.txt" }
-        |> Seq.chunkBySize down
+    |> Seq.map (fun (stepsDown, stepsRight) ->  
+        input
+        |> Seq.chunkBySize stepsDown
         |> Seq.indexed 
         |> Seq.tail
         |> Seq.map (fun (i,e) -> 
-            Seq.skip (finalIndex i right (Seq.head e).Length) (Seq.head e) 
+            Seq.skip (finalIndex i stepsRight (Seq.head e).Length) (Seq.head e) 
             |> Seq.head ) 
         |> Seq.filter (fun a -> a = '#') 
         |> Seq.length)
