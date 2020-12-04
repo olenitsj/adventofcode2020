03c3aacc0b74e75a0c6d5477681879bd8f0eb45e
Igor Olenitsj <igor.olenitsj@traxion.com>
2020-12-04 23:28:59 +0100

    feat: day 3 full

--- com.olenitsj.adventofcode/Day3.fs
+++ com.olenitsj.adventofcode/Day3.fs
@@ -2,12 +2,20 @@
 
 open System.IO
 let config  = [(1,1);(1,3);(1,5);(1,7);(2,1)]
-let finalIndex steps listLength = steps * 3 % listLength
+let finalIndex steps stepsize listLength = steps * stepsize % listLength
+
 
 
 let day3 = 
-    seq { yield! File.ReadLines @"DayThreeInput.txt" } 
-    |> Seq.indexed 
-    |> Seq.map (fun (i,e) -> Seq.skip (finalIndex i e.Length) e |> Seq.head ) 
-    |> Seq.filter (fun a -> a = '#') 
-    |> Seq.length
+    config 
+    |> Seq.map (fun (down, right) ->  
+        seq { yield! File.ReadLines @"DayThreeInput.txt" }
+        |> Seq.chunkBySize down
+        |> Seq.indexed 
+        |> Seq.tail
+        |> Seq.map (fun (i,e) -> 
+            Seq.skip (finalIndex i right (Seq.head e).Length) (Seq.head e) 
+            |> Seq.head ) 
+        |> Seq.filter (fun a -> a = '#') 
+        |> Seq.length)
+    |> Seq.fold (*) 1
